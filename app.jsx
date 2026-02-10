ce241bb80bd962dc5006e8296b98975f047328fe
aniketnepal <aniketnepal5@gmail.com>
2026-02-10 22:54:27 +1100

    ini

---
+++ app.jsx
@@ -0,0 +1,20 @@
+import { HashRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Memories from "./pages/Memories";
+import Reasons from "./pages/Reasons";
+import Valentine from "./pages/Valentine";
+import Yes from "./pages/Yes";
+
+export default function App() {
+  return (
+    <HashRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/memories" element={<Memories />} />
+        <Route path="/reasons" element={<Reasons />} />
+        <Route path="/valentine" element={<Valentine />} />
+        <Route path="/yes" element={<Yes />} />
+      </Routes>
+    </HashRouter>
+  );
+}
